2ca7af65b0ad1db603c3d718f3f11d4006585851
Dusty Phillips <dusty@phillips.codes>
2021-09-26 14:17:13 -0300

    Version bumps
    This is a mess, sorry.
    
    * Bumps yarn to yarn 2
    * Bumps rescript-zora
    * changes PTA runner to be not ugly

---
+++ tests/pretty.sh
@@ -0,0 +1,12 @@
+# pretty.sh just pipes pta to tap-diff
+# I orginally had the
+# test:watch script set to 
+#    "test:watch": "onchange --initial '{tests,src}/*.mjs' -- yarn test",
+# however it was laggy due to the yarn startup time
+# so I moved it here instead
+
+parent_path=$(dirname $(dirname $(realpath $0)))
+pta=$parent_path/node_modules/.bin/pta
+tapdiff=$parent_path/node_modules/.bin/tap-diff
+
+$pta tests/*.test.mjs | $tapdiff
